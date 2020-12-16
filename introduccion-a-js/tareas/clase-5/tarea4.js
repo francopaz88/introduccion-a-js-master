a68e9187cf2a78011bf0750a2c6bd3b2adeaa391
Franco <51251171+francopaz88@users.noreply.github.com>
2020-12-16 18:24:12 -0300

    Empiezo tarea 4, falta la ultima parte nada mas(encontrar numero repetido)

--- introduccion-a-js/tareas/clase-5/tarea4.js
+++ introduccion-a-js/tareas/clase-5/tarea4.js
@@ -7,4 +7,84 @@
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
 let $valores = document.querySelectorAll(".valores");
-console.log($valores.value);
+let $promedio = document.querySelector("#promedio");
+let $menor = document.querySelector("#menor");
+let $mayor = document.querySelector("#mayor");
+let $repetido = document.querySelector("#repetido");
+
+function promedio(array){
+    suma = 0;
+    for (let i = 0; i < array.length; i++) {
+        suma = suma + Number(array[i].innerHTML);  
+    }
+    total = suma / array.length;
+    return total;
+}
+
+promedio($valores);
+
+$promedio.innerHTML = "El promedio es " + total.toFixed(2);
+
+///////////////////////////////////////////////////////////////////
+
+function NumeroMenor(array){
+    menor = Number(array[0].innerText);
+    for (let i = 0; i < array.length; i++) {
+        if (menor > Number(array[i].innerText)) {
+            menor = Number(array[i].innerText);            
+        }
+    }
+    return menor;
+}
+
+NumeroMenor($valores);
+
+$menor.innerHTML = "El número menor es " + menor;
+
+///////////////////////////////////////////////////////////////////
+
+function NumeroMayor(array){
+    mayor = Number(array[0].innerHTML);
+    for (let i = 0; i < array.length; i++) {
+        if (mayor < Number(array[i].innerHTML)) {
+            mayor = Number(array[i].innerHTML);
+        }            
+    }
+    return mayor;
+}
+
+NumeroMayor($valores);
+
+$mayor.innerHTML = "El número mayor es " + mayor;
+
+///////////////////////////////////////////////////////////////////
+
+function numeroRepetido(array){
+    contador = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i+1; j < array.length; j++) {
+            if (Number(array[i].innerHTML) == Number(array[j].innerHTML)) {
+                console.log(array[i].innerHTML);                
+                contador++;
+            };
+            
+        };
+        
+    }
+    console.log(contador);
+}
+
+numeroRepetido($valores);
+
+// funcion calcularNumeroMasFrecuente(array):
+// numeroMasFrecuente;
+// maximasRepeticiones = 0;
+// por cada elemento del array:
+//    numeroActual = elemento;
+//    por cada otroElemento del array:
+//      si otroElemento === numeroActual
+//         repeticionesActuales++;
+//   si repeticionesActuales > maximasRepeticiones
+//     numeroMasFrecuente = numeroActual
+//     maximasRepeticiones = repeticionesActuales
+// devolver numeroMasFrecuente
